5fad13da2918cd1b804b99b41c46713505ef37d4
IAmCopyrighted <tnschmidt7@gmail.com>
2019-09-13 09:54:09 -0500

    Update Clear.java

--- src/main/java/me/capyright/Commands/Clear.java
+++ src/main/java/me/capyright/Commands/Clear.java
@@ -2,6 +2,7 @@ package me.capyright.Commands;
 
 import me.capyright.Utils.c;
 import me.capyright.Utils.s;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,6 +17,20 @@ public class Clear implements CommandExecutor {
             p.sendMessage(c.c("&c" + s.star + "&7 Your inventory has been &ccleared&7."));
         } else {
             p.sendMessage(c.c(s.error_np));
+        }
+        if(args.length == 1) {
+            if(p.isOp()) {
+                Player r = Bukkit.getPlayerExact(args[0]);
+                if(r != null) {
+                    r.getInventory().clear();
+                    r.sendMessage(c.c("&c" + s.star + "&7 Your inventory has been &ccleared&7."));
+                    p.sendMessage(c.c("&c" + s.star + "&7 You have successfully cleared&c " + r.getName() + " &7's inventory"));
+                } else {
+                    p.sendMessage(c.c(s.error_ip));
+                }
+            } else {
+                p.sendMessage(c.c(s.error_np));
+            }
         }
         return false;
     }
