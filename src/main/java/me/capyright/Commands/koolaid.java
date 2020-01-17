5a9b0368003b137016e7a49826a9fe6e1a11015d
IAmCopyrighted <tnschmidt7@gmail.com>
2020-01-16 22:01:28 -0600

    Update koolaid.java
    Version updated to current stability

--- src/main/java/me/capyright/Commands/koolaid.java
+++ src/main/java/me/capyright/Commands/koolaid.java
@@ -10,7 +10,7 @@ public class koolaid implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player) sender;
-        p.sendMessage(c.c("&aKoolaid\n&aVersion: 1.0\n&aStable: Y"));
+        p.sendMessage(c.c("&aKoolaid\n&aVersion: 1.3\n&aStable: Y"));
         p.sendMessage(c.c("&aSource: &7https://github.com/capyrightdev"));
         return false;
     }
